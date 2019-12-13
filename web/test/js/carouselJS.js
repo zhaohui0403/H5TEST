d1bd4f8362a09b3c73a83a0911e5fda195274989
zhaohui6 <zhaohui6@asiainfo.com>
2019-12-13 11:00:33 +0800

    无缝滑屏

--- web/test/js/carouselJS.js
+++ web/test/js/carouselJS.js
@@ -17,6 +17,18 @@ function addImg(arr) {
             carouselWrap.style.height = img.offsetHeight + "px";
         },100);
 
+        var point = document.querySelector(".carousel-wrap > .point-wrap");
+        if(point){
+            for (var i = 0;i<arr.length;i++){
+                if(i==0){
+                    point.innerHTML+="<span class='active'></span>";
+                }else {
+                    point.innerHTML+="<span></span>";
+                }
+            }
+            var pointSpan = document.querySelectorAll(".carousel-wrap > .point-wrap > span");
+        }
+
         //定义手指一开始的位置
         var startX = 0;
         //定义元素一开始的位置
@@ -28,6 +40,9 @@ function addImg(arr) {
 
         carouselWrap.addEventListener("touchstart",function (ev) {
             ev = ev || event;
+
+            ul.style.transition = "none";
+
             var TouchX = ev.changedTouches[0];
             startX = TouchX.clientX;
             elementX = ul.offsetLeft;
@@ -58,8 +73,46 @@ function addImg(arr) {
             }else {
                 index = Math.floor(index);
             }
+
+            if(index>0){
+                index = 0;
+            }else if(index < 1-arr.length){
+                index = 1-arr.length;
+            }
+
+            //处理圆点
+            for(var i =0;i<arr.length;i++){
+                pointSpan[i].classList.remove("active");
+            }
+            pointSpan[-index].classList.add("active");
+
+            ul.style.transition = "0.4s left";
             // index = Math.round(index);
             ul.style.left = index * (document.documentElement.clientWidth) + "px";
         })
+
+        imgScroll(arr);
     }
+}
+
+function imgScroll(arr) {
+    var index = 0;
+    var ul =  document.querySelector(".carousel-wrap > .list");
+    setInterval(function () {
+        index --;
+        if(Math.abs(index) == arr.length){
+            index = 0;
+        }
+        ul.style.transition = "0.4s left";
+        ul.style.left = index * (document.documentElement.clientWidth) + "px";
+        spanScroll(arr,Math.abs(index));
+    },2000)
+}
+
+function spanScroll(arr,index) {
+    var pointSpan = document.querySelectorAll(".carousel-wrap > .point-wrap > span");
+    for(var i =0;i<arr.length;i++){
+        pointSpan[i].classList.remove("active");
+    }
+    pointSpan[index].classList.add("active");
 }
